c1471aad7382f7d7144389628c649bf3bb3d4a90
SkyPlusik <150513344+SkyPlusik@users.noreply.github.com>
2024-06-08 22:48:59 +0300

    Update test_calculator.py

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -2,35 +2,37 @@ import pytest
 
 from main.calculator import Calculator
 
-calc = Calculator()
-
 def test_add():
-    assert calc.add(-3, 2) == -1
-    assert calc.add(0, 0) == 0
-    assert calc.add(-6, -8) == -14
-    assert calc.add(-3, 5) == 2
-    assert calc.add(5, 9) == 14
-    
+    assert Calculator.add(-3, 2) == -1
+    assert Calculator.add(0, 0) == 0
+    assert Calculator.add(-6, -8) == -14
+    assert Calculator.add(-3, 5) == 2
+    assert Calculator.add(5, 9) == 14
+
+
 def test_sub():
-    assert calc.sub(-3, 2) == -5
-    assert calc.sub(0, 0) == 0
-    assert calc.sub(-6, -8) == 2
-    assert calc.sub(6, 5) == 1
-    assert calc.sub(5, 9) == -4
+    assert Calculator.sub(-3, 2) == -5
+    assert Calculator.sub(0, 0) == 0
+    assert Calculator.sub(-6, -8) == 2
+    assert Calculator.sub(6, 5) == 1
+    assert Calculator.sub(5, 9) == -4
+
 
 def test_multy():
-    assert calc.multy(-3, 2) == -6
-    assert calc.multy(0, 0) == 0
-    assert calc.multy(4, 2) == 8
-    assert calc.multy(-6, -5) == 30
-    assert calc.multy(5, -1) == -5
+    assert Calculator.multy(-3, 2) == -6
+    assert Calculator.multy(0, 0) == 0
+    assert Calculator.multy(4, 2) == 8
+    assert Calculator.multy(-6, -5) == 30
+    assert Calculator.multy(5, -1) == -5
+
 
 def test_div():
-    assert calc.div(-6, 2) == -3
-    assert calc.div(8, 2) == 4
-    assert calc.div(30, -5) == -6
-    assert calc.div(-5, -1) == 5
+    assert Calculator.div(-6, 2) == -3
+    assert Calculator.div(8, 2) == 4
+    assert Calculator.div(30, -5) == -6
+    assert Calculator.div(-5, -1) == 5
+
 
 def test_div_fail():
     with pytest.raises(ZeroDivisionError):
-        calc.div(6, 0)
+        Calculator.div(6, 0)
