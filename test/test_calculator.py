0a80fa50aa21acd7937cac0240ec7a1d79fa081d
SkyPlusik <150513344+SkyPlusik@users.noreply.github.com>
2024-06-08 22:42:34 +0300

    Update test_calculator.py

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -4,52 +4,32 @@ from main.calculator import Calculator
 
 calc = Calculator()
 
-@pytest.mark.parametrize(
-    ('a', 'b', 'result'), [
-        (-3, 2, -1),
-        (0, 0, 0),
-        (-6, -8, -14),
-        (-3, 5, 2),
-        (5, 9, 14),
-    ]
-)
-def test_add(a, b, result):
-    assert calc.add(a, b) == result
+def test_add():
+    assert calc.add(-3, 2) == -1
+    assert calc.add(0, 0) == 0
+    assert calc.add(-6, -8) == -14
+    assert calc.add(-3, 5) == 2
+    assert calc.add(5, 9) == 14
+    
+def test_sub():
+    assert calc.sub(-3, 2) == -5
+    assert calc.sub(0, 0) == 0
+    assert calc.sub(-6, -8) == 2
+    assert calc.sub(6, 5) == 1
+    assert calc.sub(5, 9) == -4
 
-@pytest.mark.parametrize(
-    ('a', 'b', 'result'), [
-        (-3, 2, -5),
-        (0, 0, 0),
-        (-6, -8, 2),
-        (6, 5, 1),
-        (5, 9, -4),
-    ]
-)
-def test_sub(a, b, result):
-    assert calc.sub(a, b) == result
+def test_multy():
+    assert calc.multy(-3, 2) == -6
+    assert calc.multy(0, 0) == 0
+    assert calc.multy(4, 2) == 8
+    assert calc.multy(-6, -5) == 30
+    assert calc.multy(5, -1) == -5
 
-@pytest.mark.parametrize(
-    ('a', 'b', 'result'), [
-        (-3, 2, -6),
-        (0, 0, 0),
-        (4, 2, 8),
-        (-6, -5, 30),
-        (5, -1, -5),
-    ]
-)
-def test_multy(a, b, result):
-    assert calc.multy(a, b) == result
-
-@pytest.mark.parametrize(
-    ('a', 'b', 'result'), [
-        (-6, 2, -3),
-        (8, 2, 4),
-        (30, -5, -6),
-        (-5, -1, 5),
-    ]
-)
-def test_div(a, b, result):
-    assert calc.div(a, b) == result
+def test_div():
+    assert calc.div(-6, 2) == -3
+    assert calc.div(8, 2) == 4
+    assert calc.div(30, -5) == -6
+    assert calc.div(-5, -1) == 5
 
 def test_div_fail():
     with pytest.raises(ZeroDivisionError):
