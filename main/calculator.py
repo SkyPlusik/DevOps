64750929cbb71f1b70476832292a60b57df952bd
SkyPlusik <150513344+SkyPlusik@users.noreply.github.com>
2024-06-08 22:34:36 +0300

    Update calculator.py

--- main/calculator.py
+++ main/calculator.py
@@ -1,23 +1,23 @@
 class Calculator:
     def add(a, b):
-        result = 0
+        result = a + b
         # выполни сложение
         return result
 
     def sub(a, b):
-        result = 0
+        result = a - b
         # выполни вычитание
         return result
 
     def multy(a, b):
-        result = 0
+        result = a * b
         # выполни умножение
         return result
 
     def div(a, b):
         result = 0
         # выполни деление (с обработкой случая, когда b = 0)
-        if b != 0:
-            return result
+        if b != 0: result = a / b
         else:
             raise ZeroDivisionError
+        return result
